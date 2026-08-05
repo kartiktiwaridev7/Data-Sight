1802c82d647374f8a5f5262e5c260faf6ee627e8
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2026-08-05 11:58:00 +0530

    Improve the Pdf genration code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 import TechBadge from '../TechBadge';
