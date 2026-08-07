4d3e58a2df9b77c4394b967e05cae700575c6061
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2026-08-08 00:56:31 +0530

    Remove Router basename for Netlify root deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Data-Sight">
+ <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
